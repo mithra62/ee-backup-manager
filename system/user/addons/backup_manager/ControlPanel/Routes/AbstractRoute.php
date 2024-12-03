1d3cacf4e16774ac0b0f76c924ae43580332e51b
Eric Lamb <eric@mithra62.com>
2024-12-03 14:38:48 -0700

    adds permission check for ability to take backups

--- system/user/addons/backup_manager/ControlPanel/Routes/AbstractRoute.php
+++ system/user/addons/backup_manager/ControlPanel/Routes/AbstractRoute.php
@@ -10,4 +10,13 @@ abstract class AbstractRoute extends BaseRoute
      * @var string
      */
     protected $base_url = 'addons/settings/backup_manager';
+
+    public function __construct()
+    {
+        if (! ee('Permission')->can('access_utilities') && ! ee('Permission')->can('edit_channel_fields')) {
+            show_error(lang('unauthorized_access'), 403);
+        }
+
+        parent::__construct();
+    }
 }
